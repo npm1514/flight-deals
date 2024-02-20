33767d307f25bd41692c6df7172fb9bf0c3910c6
NIck Marucci <npm1514@gmail.com>
2024-02-20 15:23:44 -0700

    add key

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -4,8 +4,8 @@ export default function Page() {
         <h1>List page</h1>
         <section>
           {
-            [...new Array(8)].map(() => (
-              <div>
+            [...new Array(8)].map((item, index) => (
+              <div key={index}>
                 <a href="/flight"><div>this is a listing</div></a>
                 <p>these are the details</p>
               </div>
